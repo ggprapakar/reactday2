13f7a326084314ebd40097a42ae93439ed3ea748
ggprapakar <govindaprapakaran28@gmail.com>
2024-07-08 01:39:49 +0530

    Start new empty react project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// Imports the specified module here
+import React from 'react';
+
+// write the functions here
+const App = () => {
+
+    // write the javascripts here
+    const a = 10;
+    return (
+        // write the HTML/css/JS here or JSX here
+        // <div>
+        //     <h1>Hello!</h1> {a}
+        // </div>
+
+        // you can write the empty fragment here
+        <>
+        <h1>Hello this is empty fragment</h1>
+        </>
+        
+    );
+};
+
+// write the export function here
+export default App;
